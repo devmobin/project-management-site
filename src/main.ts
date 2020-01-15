a2680779b47b553dcc380e80a3bd0b65045795d5
devmobin <mobinheydari114@gmail.com>
2020-01-15 23:10:27 +0330

    home page rendering added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './Modules/AppModule/app.module';
+import { join } from 'path';
+import { port } from './Config/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'src', 'Views', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'src', 'Views'));
+  app.setViewEngine('ejs');
+
+  await app.listen(port);
 }
 bootstrap();
 // Kwc8rs8jYWRnQTMA
