3de89c5645954b763f70fb97b6f4bbbff9d550a5
devmobin <mobinheydari114@gmail.com>
2020-01-21 12:41:55 +0330

    refactor and readability

--- src/ioc/register.js
+++ src/ioc/register.js
@@ -6,13 +6,15 @@ const Validator = require('../modules/Shared/middleware/validator');
 const UserService = require('../modules/UserModule/user.service');
 
 const registerIoc = () => {
+  Container.register('IValidator', Validator);
+
   Container.register('IAuthService', AuthService);
+  Container.register('IUserService', UserService);
+
   Container.register('IAuthController', AuthController, 'singleton', [
     'IAuthService',
     'IUserService'
   ]);
-  Container.register('IValidator', Validator);
-  Container.register('IUserService', UserService);
 };
 
 module.exports = registerIoc;
